9720b7082a5373aecdd3bd1c09a8826afdb6b925
Bhuvan Mehra <bhuvan.mehra@qantasloyalty.com>
2019-11-04 09:49:24 +1100

    Fixed the body parser issue

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const expressGraphQL = require('express-graphql');
 const schema = require('./schema/schema');
+const bodyParser = require('body-parser')
 const app = express();
 const cors = require('cors')
 
 app.use(cors());
+app.use(bodyParser.json({ limit: '50mb' }));
 
 app.get('/', (req, res) => {
   res.json({message: 'Hello Express!!'})
@@ -13,7 +15,7 @@ app.get('/', (req, res) => {
 app.use(
   '/graphql',
   expressGraphQL({
-    schema,
+    schema: schema,
     graphiql: true
   })
 );
